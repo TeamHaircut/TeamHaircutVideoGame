a0bb0162ba3354a8af8eb214a7503b249a1bdf17
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-04-11 22:55:30 -0400

    updated IntroState

--- TeamHaircutGameV2/src/states/IntroState.java
+++ TeamHaircutGameV2/src/states/IntroState.java
@@ -11,13 +11,13 @@ import org.newdawn.slick.state.*;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
+import dimensions.Ints;
 import strings.Strings;
 import supportclasses.CustomAnimator;
 
 public class IntroState extends BasicGameState{
 	
 	//use 6 points and its multipliers: 12,18,24,30,36,42,48,54,60...
-	private static final int FONTSIZE = 30;
 	private static int counter;
 	private static Font font;
 	private CustomAnimator ca;
@@ -28,15 +28,15 @@ public class IntroState extends BasicGameState{
    }
    
    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-	   font = new TrueTypeFont(new java.awt.Font(Strings.FONTNAME, java.awt.Font.PLAIN, FONTSIZE), false);
+	   font = new TrueTypeFont(new java.awt.Font(Strings.FONTNAME, java.awt.Font.PLAIN, Ints.FONTSIZE+12), false);
    }
    
    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 	   g.setBackground(Color.black);
-	   font.drawString((ScrRes.getWidth(2))-((FONTSIZE/2)*15), 
-			   ScrRes.getHeight(2), "TEAMHAIRCUT.ORG", ca.getColor()); 
-	   font.drawString((ScrRes.getWidth(2))-((FONTSIZE/2)*10), 
-			   (ScrRes.getHeight(2))+FONTSIZE+1, " PRESENTS ", ca.getColor());
+	   font.drawString((ScrRes.getWidth(2))-(((Ints.FONTSIZE+12)/2)*15), 
+			   ScrRes.getHeight(2), "TEAMHAIRCUT.ORG", ca.getColor());//TEAMHAIRCUT.ORG 
+	   font.drawString((ScrRes.getWidth(2))-(((Ints.FONTSIZE+12)/2)*10), 
+			   (ScrRes.getHeight(2))+(Ints.FONTSIZE+12)+1, " PRESENTS ", ca.getColor());
    }
    
    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
@@ -50,4 +50,4 @@ public class IntroState extends BasicGameState{
    public int getID(){
       return -4;
    }
-}
+}//[end IntroState]
