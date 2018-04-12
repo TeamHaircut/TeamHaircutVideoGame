75c406bd2e5e8d840eaa9d586b8e33f4be1becd5
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-04-11 20:07:33 -0400

    updated game driver

--- TeamHaircutGameV2/src/states/GameDriver.java
+++ TeamHaircutGameV2/src/states/GameDriver.java
@@ -9,9 +9,7 @@ import org.newdawn.slick.state.*;
 import strings.Strings;
 
 public class GameDriver extends StateBasedGame{
-   
-   //private static final String GAMENAME = "Team Haircut: The Video Game";
-   
+      
    public GameDriver(String gameName){
       super(gameName);
       this.addState(new IntroState());
@@ -39,33 +37,25 @@ public class GameDriver extends StateBasedGame{
       /*
        * use GameState.INTROSTATE, to start at beginning
        */
-      this.enterState(GameState.INTROSTATE);
-      //this.enterState(GameState.TITLESTATE);
-      //this.enterState(GameState.SELECTSTATE);
-      //this.enterState(GameState.STARTSTATE);
-      //this.enterState(GameState.LEVEL_1_1);
-      //this.enterState(GameState.LEVEL_Test);
+//      this.enterState(GameState.INTROSTATE);
+//      this.enterState(GameState.TITLESTATE);
+//      this.enterState(GameState.SELECTSTATE);
+//      this.enterState(GameState.STARTSTATE);
+      this.enterState(GameState.LEVEL_1_1);
+//      this.enterState(GameState.LEVEL_Test);
    }
    
    public static void main(String[] args) {
       AppGameContainer appgc;
       try{
          appgc = new AppGameContainer(new GameDriver(Strings.GAMENAME));
-//       ScreenResolution.setScreenHeight(480);
-//       ScreenResolution.setScreenWidth(980);
-//       appgc.setDisplayMode(980, 480, false);// old setting
-//       appgc.setDisplayMode(1360, 768, true);//samsung
-       
-//       appgc.setDisplayMode(1920, 1080, true);//full hd 3840
-         
          ScrRes.setHeight(appgc.getScreenHeight());
          ScrRes.setWidth(appgc.getScreenWidth());
-//         appgc.setDisplayMode(ScrRes.getWidth(), ScrRes.getHeight(), true);//true for full screen
-         appgc.setDisplayMode(ScrRes.getWidth(), ScrRes.getHeight(), false);//true for full screen
+         appgc.setDisplayMode(ScrRes.getWidth(), ScrRes.getHeight(), true);
          
 //       alt-tab to exit fullscreen
-         //appgc.setFullscreen(true);//uncomment for full screen
-         appgc.setShowFPS(false);//true
+         appgc.setFullscreen(true);//uncomment for full screen
+         appgc.setShowFPS(true);//true
          appgc.start();
       }catch(SlickException e){
          e.printStackTrace();
