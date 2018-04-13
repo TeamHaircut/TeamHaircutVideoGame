a84e3169ca7678565ca15ca9c86e83349e76429e
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-04-12 22:19:47 -0400

    updated start state

--- TeamHaircutGameV2/src/states/StartState.java
+++ TeamHaircutGameV2/src/states/StartState.java
@@ -7,9 +7,9 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 import org.newdawn.slick.state.transition.EmptyTransition;
 
+import dimensions.Ints;
+
 public class StartState extends BasicGameState{
-	//use 6 points and its multipliers: 12,18,24,30,36,42,48,54,60...
-	private static final int FONTSIZE = 24;
 	private static Font font;
 	
 	private int counter;
@@ -19,19 +19,19 @@ public class StartState extends BasicGameState{
 	}
    
    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-       font = new TrueTypeFont(new java.awt.Font(Strings.FONTNAME, java.awt.Font.BOLD, FONTSIZE), false);
+       font = new TrueTypeFont(new java.awt.Font(Strings.FONTNAME, java.awt.Font.BOLD, Ints.FONTSIZE+6), false);
        HeroState.setState(HeroState.STATE_ALIVE);
    }
    
    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 	   g.setBackground(Color.black);
 	   if (HeroState.getIndex()== HeroState.DAMIAN) {
-		   Image damianImage = new Image("res/setupImages/damianselectA.png");
+		   Image damianImage = new Image(Strings.SELECT_STATE_IMAGE_0);
 		   g.drawImage(damianImage, (ScrRes.getWidth(2))-(damianImage.getWidth()), 
 				   (ScrRes.getHeight(2))-(damianImage.getHeight()/2));
 	   }
 	   if (HeroState.getIndex()==HeroState.DAN) {
-		   Image danImage = new Image("res/setupImages/danselectA.png");
+		   Image danImage = new Image(Strings.SELECT_STATE_IMAGE_1);
 		   g.drawImage(danImage, (ScrRes.getWidth(2))-(danImage.getWidth()), 
 				   (ScrRes.getHeight(2))-(danImage.getHeight()/2));
 	   }
@@ -42,7 +42,7 @@ public class StartState extends BasicGameState{
    
    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 	   counter += delta;
-	   if (counter >= 1000) {
+	   if (counter >= 1000) {//1000
 		   counter = 0;
 		   switch(GameState.getCurrentState()) {
 		   	case GameState.LEVEL_1_1:
@@ -55,11 +55,10 @@ public class StartState extends BasicGameState{
 			default:
 				break;
 		   }
-
 	   }
 	}
    
    public int getID(){
       return -1;
    }
-}
+}//[end StartState]
